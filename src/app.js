bf17130d25e763979c89ffb25bf71055c08481ee
Siddheshinde <siddhesh19205@gmail.com>
2026-01-12 11:51:35 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import { use } from "react"
 
 const app= express()
 
-app.cora(cors({
+app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials:true
 }))
@@ -14,5 +13,14 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
 
 export{app}
